d53fe85d0f1f038f6d60689adb7d4babfbe085f6
sphinxxxxxxxxx <73945837+sphinxxxxxxxxx@users.noreply.github.com>
2026-04-29 19:44:42 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -6,6 +6,15 @@ contextBridge.exposeInMainWorld('electronAPI', {
     previewMode: (config) => ipcRenderer.send('preview-mode', config),
     saveLayout: (layout) => ipcRenderer.send('save-layout', layout),
     endRestEarly: () => ipcRenderer.send('end-rest-early'),
+    
+    // 新增：工作面板控制
+    hideWindow: () => ipcRenderer.send('hide-window'),
+    minWindow: () => ipcRenderer.send('min-window'),
+    restartWork: () => ipcRenderer.send('restart-work'),
+    
+    // 监听事件
     onTimerUpdate: (callback) => ipcRenderer.on('timer-update', (event, time) => callback(time)),
+    onMainTimerTick: (callback) => ipcRenderer.on('main-timer-tick', (event, time) => callback(time)),
+    onStateSync: (callback) => ipcRenderer.on('state-sync', (event, data) => callback(data)),
     onCleanup: (callback) => ipcRenderer.on('cleanup-before-destroy', () => callback())
 });
