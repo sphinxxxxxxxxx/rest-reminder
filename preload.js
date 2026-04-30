21267ce3ac945779e61ebfbb3996806d729a193e
sphinxxxxxxxxx <73945837+sphinxxxxxxxxx@users.noreply.github.com>
2026-04-30 10:47:56 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,18 +1,18 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    uploadMedia: () => ipcRenderer.invoke('upload-media'),
+    uploadMedia: (isSecondary) => ipcRenderer.invoke('upload-media', isSecondary),
     startWork: (config) => ipcRenderer.send('start-work', config),
     previewMode: (config) => ipcRenderer.send('preview-mode', config),
-    saveLayout: (layout) => ipcRenderer.send('save-layout', layout),
-    endRestEarly: () => ipcRenderer.send('end-rest-early'),
     
-    // 新增：工作面板控制
+    updateLayout: (data) => ipcRenderer.send('update-layout', data),
+    exitAdjustMode: () => ipcRenderer.send('exit-adjust-mode'),
+    
+    endRestEarly: () => ipcRenderer.send('end-rest-early'),
     hideWindow: () => ipcRenderer.send('hide-window'),
     minWindow: () => ipcRenderer.send('min-window'),
     restartWork: () => ipcRenderer.send('restart-work'),
     
-    // 监听事件
     onTimerUpdate: (callback) => ipcRenderer.on('timer-update', (event, time) => callback(time)),
     onMainTimerTick: (callback) => ipcRenderer.on('main-timer-tick', (event, time) => callback(time)),
     onStateSync: (callback) => ipcRenderer.on('state-sync', (event, data) => callback(data)),
